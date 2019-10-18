45370561864621b9d908f2a2faa201aa4abe9680
Natalie Montano <nat.montano366@gmail.com>
2019-10-17 20:04:27 -0700

    created conditional statements for user inputs required and created Logic();

--- Index.js
+++ Index.js
@@ -64,4 +64,47 @@ var computerWord = new Word(randomWord);
 
 var requireNewWord= false;
 var incorrectLetters = [];
+var correctLetters = [];
+
+var guessesLeft = 10;
+
+//selects in random from the u.s names array
+function theLogic(){
+    if(requireNewWord){
+        var randomIndex = Math.floor(Math.random()* UnitedStates.length);
+        var randomWord = UnitedStates[randomIndex];
+
+        computerWord = new Word(randomWord);
+
+        requireNewWord = false;
+    }
+    var wordComplete =  [];
+    if (wordComplete.includes(false)){
+        inquire.prompt({
+            {
+                type:"input",
+                message: "Select letter from A to Z",
+                name: "userInput"
+            }
+        }).then(function(input){
+            if(!letterArray.includes(input.userInput) || 
+            input.userInput. length > 1
+            ){
+                console.log("\nPlease try again.\n");
+                theLogic();
+            }else{
+                if(
+                    incorrectLetters.includes(input.userInput) || 
+                    correctLetters.includes(input.userInput) ||
+                    input.userInput === ""
+                ){
+                    console.log("\nAlready Guessed or Nothing was entered\n");
+                    theLogic();
+                }
+            }
+        }); 
+    }else {
+        console.log("YOU WIN!!! \n")
+    }
+}
 
